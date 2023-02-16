e0e978ae1926a1eef52d49388a4e3cc53b5fbdb5
PavelKotlov <pavelkotlov@gmail.com>
2023-02-15 21:49:07 -0800

    Refactored wordsearch,js to complete all tests in test_wordsearch.js

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,12 +15,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present and horizontal", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,8 +31,74 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
-
+    ], 'SEINFELD');
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present and vertical", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LARRY');
+    assert.isTrue(result);
+  });
+
+  // What about the case where the word matrix is an empty array?
+  it("should return false if the array is empty", function() {
+    const result = wordSearch([], 'LARRY');
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is present backwards and hoirzontal", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'T'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'A'],
+      ['E', 'Z', 'K', 'F', 'Q', 'T', 'A', 'C'],
+    ], 'CAT');
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present backwards and vertical", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'O'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'L'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'L'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'E'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'H'],
+    ], 'HELLO');
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present and diagnoal", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'D', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'O', 'Y', 'E', 'R', 'O'],
+      ['B', 'F', 'R', 'E', 'G', 'E', 'Y', 'L'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'L'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'E'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'H'],
+    ], 'DOG');
+    assert.isTrue(result);
+  });
+
 });
